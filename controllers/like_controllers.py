8875a853357e7d80767dd50889f95cf46e38d6ef
kron3499 <kron3499@gmail.com>
2022-11-03 19:32:54 +0200

    4

--- controllers/like_controllers.py
+++ controllers/like_controllers.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, request, jsonify
+from sqlalchemy import and_
 
 from app import db
 from model.like import Like
+from model.post import Post
 from token_required import token_required
 
 like_control: Blueprint = Blueprint('like', __name__, url_prefix='/likes')
@@ -11,9 +13,15 @@ like_control: Blueprint = Blueprint('like', __name__, url_prefix='/likes')
 @token_required
 def create_like(current_user, post_id):
     liked = request.json.get("liked")
-    like = Like(post_id=post_id, like_user_id=current_user.id, liked=liked)
+    post = Post.query.filter(Post.id == post_id).first()
+    if not post:
+        return "post not found", 400
+    if not liked:
+        like = Like.query.filter(and_(Like.post_id == post_id, Like.like_user_id == current_user.id)).delete()
+    else:
+        like = Like(post_id=post_id, like_user_id=current_user.id, liked=liked)
 
-    db.session.add(like)
-    db.session.commit()
+        db.session.add(like)
+        db.session.commit()
 
-    return jsonify({'like.id': like.id})
+        return jsonify({'like.id': like.id})
